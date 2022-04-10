b5a2a52c78d118a35da1013249b8265461f2dd97
quocduyvu6262 <quocduyvu6262@gmail.com>
2022-04-10 01:09:49 -0700

    test

--- public/calculator.js
+++ public/calculator.js
@@ -1,41 +1,56 @@
-const Datastore = require('nedb');
-const database = new Datastore('/Users/duyvuquoc/Documents/EmotionalDetector_SDHack2022/database.db');
-database.loadDatabase();
+function getData(){
+    const Datastore = require('nedb');
+    const database = new Datastore('/Users/duyvuquoc/Documents/EmotionalDetector_SDHack2022/database.db');
+    database.loadDatabase();
 
-var datas_happy = new Array();
-var datas_sad = new Array();
-var datas_angry = new Array();
+    var datas_happy = new Array();
+    var datas_sad = new Array();
+    var datas_angry = new Array();
+    let mean_emotions = {}
+    let sortable = [];
+    database.find({happy: {$gte:0.20}},(err,docs)=>{
+        if(err){
+            return;
+        }
+        for(item of docs){
+            datas_happy.push(item.happy);
+        }
+    })
+    
+    database.find({sad: {$gte:0.20}},(err,docs)=>{
+        if(err){
+            return;
+        }
+        for(item of docs){
+            datas_sad.push(item.sad);
+        }
+    })
+    
+    database.find({angry: {$gte:0.02}},(err,docs)=>{
+        if(err){
+            return;
+        }
+        for(item of docs){
+            datas_angry.push(item.angry);
+        }
+    })
+    
+    const average = (array) => array.reduce((a, b) => a + b) / array.length;
+    
+    setTimeout(function() {
+        sortable.push(['happy', average(datas_happy)]);
+        sortable.push(['sad', average(datas_sad)]);
+        sortable.push(['angry', average(datas_angry)]);
+        sortable.sort(function(a,b){
+            return a[1] - b[1];
+        });
+        console.log(sortable);
+        return sortable;
+    }, 1000);
+}
 
-database.find({happy: {$gte:0.20}},(err,docs)=>{
-    if(err){
-        return;
-    }
-    for(item of docs){
-        datas_happy.push(item.happy);
-    }
-})
+function change(){
+    getData();
+}
+change();
 
-database.find({sad: {$gte:0.20}},(err,docs)=>{
-    if(err){
-        return;
-    }
-    for(item of docs){
-        datas_sad.push(item.sad);
-    }
-})
-
-database.find({angry: {$gte:0.02}},(err,docs)=>{
-    if(err){
-        return;
-    }
-    for(item of docs){
-        datas_angry.push(item.angry);
-    }
-})
-
-const average = (array) => array.reduce((a, b) => a + b) / array.length;
-setTimeout(function() {
-    console.log(average(datas_happy)); // this will (probably) be defined, since nedb is fast & shouldn't take a full second
-    console.log(average(datas_sad));
-    console.log(average(datas_angry));
-}, 1000);
