3d921d8c03d8817487e402c7b7b97426664b98b3
quocduyvu6262 <quocduyvu6262@gmail.com>
2022-04-10 05:20:24 -0700

    update server file for cloudfare

---
+++ dist/index.9af9b17d.js
@@ -0,0 +1,2 @@
+function togglePopup(){document.getElementById("popup-1").classList.toggle("active")}function secondtogglePopup(){document.getElementById("popup-2").classList.toggle("active")}
+//# sourceMappingURL=index.9af9b17d.js.map
