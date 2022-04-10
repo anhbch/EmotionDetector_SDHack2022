8648bc0f78cc56231fdb07a3e208f1dfca81917c
anhbch <90117476+anhbch@users.noreply.github.com>
2022-04-09 20:54:05 -0700

    Updated time countdown

--- public/timer.js
+++ public/timer.js
@@ -11,13 +11,13 @@ function startTimer(duration, display) {
 
         if (--timer < 0) {
             timer = 0;
-            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+            //timer = duration; // uncomment this line to reset timer automatically after reaching 0
         }
     }, 1000);
 }
 
 window.onload = function () {
-    var time = 60 / 2, // your time in seconds here
+    var time = 30 * 60, // your time in seconds here
         display = document.querySelector('#clock');
     startTimer(time, display);
 };
