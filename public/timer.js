27f937b67e70f7c9817269dc5938f9372d4a897a
anhbch <90117476+anhbch@users.noreply.github.com>
2022-04-10 01:56:25 -0700

    Add reset timer

--- public/timer.js
+++ public/timer.js
@@ -11,22 +11,30 @@ function startTimer(duration, display) {
 
         display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = 0;
-            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
-        }
+        // if (--timer < 0) {
+        //     timer = 0;
+        //     // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+        // }
         // timer--;
-        // if (timer >= 0) {
-        //     span = document.getElementById("clock");
-        // }
-        // if (timer === 0) {
-        //     alert('sorry, out of time');
-        //     clearInterval(timer);
-        // }
+        if (--timer < 0) {
+            timer = 0
+        }
+        // Need to update after calculate emotion
+        if (timer === 0) {
+            alert('sorry, out of time');
+            clearInterval(timer);
+        }
     }, 1000);
 };
 
 function start() {
+    var time = 30 * 60, // your time in seconds here
+    display = document.querySelector('#clock');
+    startTimer(time, display);
+};
+
+function reset() {
+    clearInterval(myInterval);
     var time = 30 * 60, // your time in seconds here
     display = document.querySelector('#clock');
     startTimer(time, display);
