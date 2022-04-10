9245721e2d8745b0fb8bab0ea5ed92a1dd384c6a
anhbch <90117476+anhbch@users.noreply.github.com>
2022-04-10 00:27:53 -0700

    Start worked

--- public/timer.js
+++ public/timer.js
@@ -1,4 +1,3 @@
-const { start } = require("repl");
 
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
@@ -11,18 +10,26 @@ function startTimer(duration, display) {
 
         display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = 0;
-            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+        // if (--timer < 0) {
+        //     timer = 0;
+        //     // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+        // }
+        timer--;
+        if (timer >= 0) {
+            span = document.getElementById("clock");
+        }
+        if (timer === 0) {
+            alert('sorry, out of time');
+            clearInterval(timer);
         }
     }, 1000);
-}
-
-start.onclick() = function start() {
-    var time = 30 * 60, // your time in seconds here
-        display = document.querySelector('#clock');
-    startTimer(time, display);
 };
+
+window.addEventListener('click',function () {
+    var time = 30 * 60, // your time in seconds here
+    display = document.querySelector('#clock');
+    startTimer(time, display);
+});
 
 
 
